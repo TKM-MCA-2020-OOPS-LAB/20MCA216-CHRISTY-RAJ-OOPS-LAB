e8c6f6347597b4af56ba9b0d388235191a071134
christy951 <76025052+christy951@users.noreply.github.com>
2021-06-03 16:31:31 +0530

    Commit

---
+++ LABCYCLE4/co1 q1 Main.java
@@ -0,0 +1,27 @@
+package JAVA;
+import graphics.figures;
+public class Main
+{
+
+	public static void main(String[] args) 
+	{
+		Rectangle obj=new Rectangle();
+		obj.input();
+		obj.area();
+		Circle obj1=new Circle();
+		obj1.input();
+		obj1.area();
+	     Square obj2=new Square();
+	    obj2.input();
+		obj2.area();
+		Triangle obj3=new Triangle();
+		obj3.input();
+		obj3.area();
+	}
+     
+       
+      
+}
+
+	
+	
