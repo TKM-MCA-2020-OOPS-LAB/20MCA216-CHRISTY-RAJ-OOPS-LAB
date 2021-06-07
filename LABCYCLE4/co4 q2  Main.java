79f1f9d16360a96d3b2aec3c9fd4e5231e404603
christy951 <76025052+christy951@users.noreply.github.com>
2021-06-07 10:08:49 +0530

    COMMIT

---
+++ LABCYCLE4/co4 q2  Main.java
@@ -0,0 +1,23 @@
+package JAVA;
+import Arithematic.Math;
+public class Main
+{
+
+	public static void main(String[] args) 
+	{
+
+		Math obj = new Math();
+		
+		System.out.println("   ARITHMETIC OPERATIONS   ");
+    	obj.addition();
+    	obj.subtraction();
+    	obj.multiplication();
+    	obj.division();
+	}
+     
+       
+      
+}
+
+	
+	
