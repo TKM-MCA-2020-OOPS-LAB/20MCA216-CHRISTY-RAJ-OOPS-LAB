ded3cc4705d6237b313d14753c18d5b30753ea54
christy951 <76025052+christy951@users.noreply.github.com>
2021-06-09 20:04:15 +0530

    COMMIT

---
+++ LABCYCLE4/q3/co4 q3.java
@@ -0,0 +1,32 @@
+package JAVA;
+
+import java.util.Scanner;
+public class authentication{
+
+public static void main(String[] args) {
+	 Scanner s1 = new Scanner(System.in);
+	  String Username, Password;
+	  
+	  System.out.print("Enter username :: ");
+	  Username = s1.nextLine();
+	  
+	  System.out.print("Enter password :: ");
+	  Password = s1.nextLine();
+	  
+     int name=Username.length();
+	  int length=Password.length();
+try {
+if(name>5 && length>5) {
+	System.out.println("CONGRAGULATION!");
+}
+	else {
+		throw new invalidexception("Sorry your information are invalid");
+	}
+}
+	catch(invalidexception e){
+		System.out.println(e.getMessage());
+		
+	}
+	
+}
+}
