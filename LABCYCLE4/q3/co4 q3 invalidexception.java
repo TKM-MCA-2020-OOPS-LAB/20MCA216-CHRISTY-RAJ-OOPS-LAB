656d7c9d32ca162aa744935f0fa7161c272c6cd9
christy951 <76025052+christy951@users.noreply.github.com>
2021-06-09 20:05:31 +0530

    COMMIT

---
+++ LABCYCLE4/q3/co4 q3 invalidexception.java
@@ -0,0 +1,12 @@
+package JAVA;
+
+public class invalidexception extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+ invalidexception(String s){
+        super(s);
+    }
+}
