18f1cb5832c4ff8a3be7e5a93180014e9e894350
christy951 <76025052+christy951@users.noreply.github.com>
2021-06-19 23:29:17 +0530

    COMMIT

---
+++ LABCYCLE4/q7/co4 q7.java
@@ -0,0 +1,82 @@
+package JAVA;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class producercosumer {
+
+	public static void main(String[] args) {
+		List<Integer>  sharedList = new ArrayList<Integer>();
+		Thread thread1=new Thread(new producer(sharedList));
+		Thread thread2=new Thread(new consumer(sharedList));
+		thread1.start();
+		thread2.start();
+		
+
+	}
+}
+
+
+class producer implements Runnable{
+	List<Integer>  sharedList = null;
+	final int MAX_SIZE=5;
+	private int i=0;
+	
+	public producer(List<Integer> sharedList) {
+		super();
+		this.sharedList = sharedList;
+		
+	}
+	public void run() {
+		while(true) {
+			try {
+			produce(i++);
+			}catch(InterruptedException exception) {
+	}
+}
+	}
+public void produce(int i) throws InterruptedException{
+	synchronized(sharedList) {
+		while(sharedList.size()==MAX_SIZE) {
+			System.out.println("sharedList is full...................waiting for the consumer to consume");
+			sharedList.wait();
+		}
+	}
+   synchronized(sharedList) {
+	   System.out.println("producer produce object"+i);
+	   sharedList.add(i);
+	   Thread.sleep(1000);
+	   sharedList.notify();
+   }
+}
+}
+class consumer implements Runnable{
+	List<Integer>  sharedList = null;
+	public consumer(List<Integer> sharedList) {
+		super();
+		this.sharedList = sharedList;
+		
+	}
+	public void run() {
+		while(true) {
+			try {
+			consume();
+			}catch(InterruptedException exception) {
+	}
+}
+	}
+public void consume() throws InterruptedException{
+	synchronized(sharedList) {
+		while(sharedList.isEmpty()) {
+			System.out.println("sharedList is Empty...................waiting for the producer to produce the object");
+			sharedList.wait();
+		}
+	}
+   synchronized(sharedList) {
+        Thread.sleep(1000);
+        System.out.println("consume the element"+sharedList.remove(0));
+	   sharedList.notify();
+   }
+}
+}
